6c3c91a69109db92bd48f938398f0cbcf7da1bf4
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-01-01 16:21:54 +0200

    use store Method & x-init attribute 🚀

--- basics/app.js
+++ basics/app.js
@@ -6,4 +6,17 @@ document.addEventListener("alpine:init", () => {
       this.open = !this.open
     }
   }))
+
+  Alpine.store("cart", {
+    items: [
+      { name: "Apple", price: 1.99 },
+      { name: "Orange", price: 2.99 },
+      { name: "Banana", price: 3.99 },
+      { name: "Pineapple", price: 4.99 },
+      { name: "Strawberry", price: 5.99 }
+    ],
+    add(item) {
+      this.items.push(item)
+    }
+  })
 })
